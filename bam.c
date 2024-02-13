85fbc9323df049073988e88b7204efbb91875b42
Mohitrawal7 <rawalmohit12@gmail.com>
2024-02-13 21:51:06 +0545

    Assignment

---
+++ bam.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main() {
+int i,j,p,temp;
+int no[]={1,2,3,5,4,5,7,9,10,8};
+printf("ascending order\n");
+for (i=0;i<10;i++){
+    for(j=i+1;j<10;j++){
+        if(no[i]>no[j]){
+            temp=no[i];
+            no[i]=no[j];
+            no[j]=temp;
+        }
+    }
+    }
+for (i=0;i<10;i++){
+    for(j=i+1;j<10;j++){
+        if(no[i]==no[j]){
+            printf("%d is repeated\n",no[i]);
+p=i+2;
+}
+}
+}
+no[p-1]=p;
+printf("%d\n",no[p]);
+for (i=0;i<10;i++){
+        printf("%d\t",no[i]);
+}
+return 0;
+}
